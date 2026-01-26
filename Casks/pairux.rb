444a7483b25e4568a008495b75c64a157cd2cf64
Anthony Ettinger <anthony@chovy.com>
2026-01-26 04:33:39 -0800

    Update pairux to 0.1.7

---
+++ Casks/pairux.rb
@@ -0,0 +1,36 @@
+cask "pairux" do
+  version "0.1.7"
+
+  on_arm do
+    sha256 "06be227c9a51dfdcdb62eccc4e0c5049b2730c9bcf264dd462433a1a2633b4f7"
+    url "https://github.com/profullstack/pairux.com/releases/download/v#{version}/PairUX-#{version}-arm64.dmg",
+        verified: "github.com/profullstack/pairux.com/"
+  end
+
+  on_intel do
+    sha256 "33a0276fba1d6eee063f11fc62efe8396a8835ca0bf0cf4bc66eba0b50e674a9"
+    url "https://github.com/profullstack/pairux.com/releases/download/v#{version}/PairUX-#{version}-x64.dmg",
+        verified: "github.com/profullstack/pairux.com/"
+  end
+
+  name "PairUX"
+  desc "Collaborative screen sharing with remote control"
+  homepage "https://pairux.com"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "PairUX.app"
+
+  zap trash: [
+    "~/Library/Application Support/PairUX",
+    "~/Library/Caches/com.pairux.desktop",
+    "~/Library/Preferences/com.pairux.desktop.plist",
+    "~/Library/Saved Application State/com.pairux.desktop.savedState",
+  ]
+end
