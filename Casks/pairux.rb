b84ec5ab79140d20cfced8679590ce053373c993
Anthony Ettinger <anthony@chovy.com>
2026-02-23 09:04:54 +0000

    Update pairux to 0.5.14

--- Casks/pairux.rb
+++ Casks/pairux.rb
@@ -1,14 +1,14 @@
 cask "pairux" do
-  version "0.5.13"
+  version "0.5.14"
 
   on_arm do
-    sha256 "9887fcc10664472fbe14304e85ad73f7c968566bbc7803bc962f4c60552b3c47"
+    sha256 "2857cc6264304ecf87e1937b6a50f3ce9419c80db659e466f4503bd80a08e6fb"
     url "https://github.com/profullstack/pairux.com/releases/download/v#{version}/PairUX-#{version}-arm64.dmg",
         verified: "github.com/profullstack/pairux.com/"
   end
 
   on_intel do
-    sha256 "0edb7fc86a2ae1612cec63c4d7938048781107780deaf6c984c32b7c90e3b5f4"
+    sha256 "a9c4468801214ae6b62f638bf69b28872585a91452130aac333e988723239db3"
     url "https://github.com/profullstack/pairux.com/releases/download/v#{version}/PairUX-#{version}-x64.dmg",
         verified: "github.com/profullstack/pairux.com/"
   end
