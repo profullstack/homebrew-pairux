ae33250c1486f0141a347cd5b3fae9a6e709478e
Anthony Ettinger <anthony@chovy.com>
2026-06-23 04:03:38 -0700

    Update pairux to 0.8.8

--- Casks/pairux.rb
+++ Casks/pairux.rb
@@ -1,14 +1,14 @@
 cask "pairux" do
-  version "0.8.7"
+  version "0.8.8"
 
   on_arm do
-    sha256 "8c8cc5f370055b11051427835f314bd7cb21bbf556d3a7e44999967961afda20"
+    sha256 "e76893e90c1dfbd54e808baded7d4f8385513a04d5718899f4cbbba72c3053cb"
     url "https://github.com/profullstack/pairux.com/releases/download/v#{version}/PairUX-#{version}-arm64.dmg",
         verified: "github.com/profullstack/pairux.com/"
   end
 
   on_intel do
-    sha256 "817bcf8699f11121a065d8ded3ba94c85a0980ab8965bc9c0907010e5aeb40ae"
+    sha256 "c25b6272aff7fb642cf8271ae441b7ec3dfef58923b704d53be79ba6bf53f59b"
     url "https://github.com/profullstack/pairux.com/releases/download/v#{version}/PairUX-#{version}-x64.dmg",
         verified: "github.com/profullstack/pairux.com/"
   end
