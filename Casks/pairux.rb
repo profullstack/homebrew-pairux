b5ac9df76373b46603d483b2536c88e28f455757
Anthony Ettinger <anthony@chovy.com>
2026-07-10 02:35:29 -0700

    Update pairux to 0.9.1

--- Casks/pairux.rb
+++ Casks/pairux.rb
@@ -1,14 +1,14 @@
 cask "pairux" do
-  version "0.9.0"
+  version "0.9.1"
 
   on_arm do
-    sha256 "b8442a48e24295982d8c60e698dc3829cd3a4ff2a22d7bb838e614c79ea3d0cf"
+    sha256 "7b75ab793b60548bb6bd54c50e88a4b08faa8504483c414ce78d06ed1dd7c1c0"
     url "https://github.com/profullstack/pairux.com/releases/download/v#{version}/PairUX-#{version}-arm64.dmg",
         verified: "github.com/profullstack/pairux.com/"
   end
 
   on_intel do
-    sha256 "cb121d04c4850d611f9634dab6791bb5393b324cf2284258a5d144f86ed64144"
+    sha256 "499f74c1392bf71e6cc76350ce32fd92c6a1af693ac8dec1f3c283dee3486c5b"
     url "https://github.com/profullstack/pairux.com/releases/download/v#{version}/PairUX-#{version}-x64.dmg",
         verified: "github.com/profullstack/pairux.com/"
   end
